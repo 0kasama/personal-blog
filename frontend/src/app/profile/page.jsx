a26b23e31d6b1047211df6b74e7bd8a81abdd9b3
0kasama <okaajyad@gmail.com>
2024-07-05 15:00:18 +0700

    feat(frontend): profile

---
+++ frontend/src/app/profile/page.jsx
@@ -0,0 +1,9 @@
+import UserProfile from "@/components/UserProfile";
+
+export default function UserPage() {
+  return (
+    <div className='container mx-auto h-screen flex flex-col items-center justify-center'>
+      <UserProfile />
+    </div>
+  );
+}
